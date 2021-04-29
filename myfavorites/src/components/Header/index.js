f9c6ca04e47270f50927ab17afb9f093ebe1ed33
saulofilho <saulosilvaf@gmail.com>
2021-04-29 20:14:52 -0300

    [fix] header width

--- myfavorites/src/components/Header/index.js
+++ myfavorites/src/components/Header/index.js
@@ -2,8 +2,6 @@ import { Link } from 'react-router-dom';
 import { Layout, Row, Col } from 'antd';
 import { HeartTwoTone, UserOutlined } from '@ant-design/icons';
 
-import './styles.css';
-
 const { Header } = Layout;
 
 const styleHeader = { background: '#bae7ff', padding: '0 0' };
@@ -15,11 +13,18 @@ const styleColMenu = {
 const styleMenuLink = {
   paddingRight: '20px',
 };
+const styleWrapper = {
+  maxWidth: '900px',
+  width: '100%',
+  margin: '0 auto',
+  height: '100%',
+  padding: '0 20px',
+};
 
 export default function HeaderWrapper() {
   return (
     <Header style={styleHeader}>
-      <div className="style-wrapper">
+      <div style={styleWrapper}>
         <Row>
           <Col xs={12} lg={20}>
             <Link to="/">
